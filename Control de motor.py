a93254088dd7bf489a11df6741011d5dbf6d51b2
JaelDp13 <146679309+JaelDp13@users.noreply.github.com>
2023-10-22 20:36:12 -0600

    Update Control de motor.py

--- Control de motor.py
+++ Control de motor.py
@@ -1,25 +1,76 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
+in1 = 24
+in2 = 23
+en = 25
+temp1 = 1
 
-GPIO.setup(8, GPIO.OUT)
-GPIO.setup(10, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(en, GPIO.OUT)
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+p = GPIO.PWM(en, 1000)
+p.start(25)
+print("\n")
+print("La velocidad y direccion del Motor")
+print("C-Correr A-Alto Av-Avanzar R-Reversa L-Low M-Medium H-High E-Exit")
+print("\n")
 
-for i in range(5):
-    # Giro de motor por 5 segundos
-    print("Girando el motor")
-    GPIO.output(8, GPIO.HIGH)
-    GPIO.output(10, GPIO.LOW)
-    time.sleep(5)
+while (1):
 
-    # Gira de motor en sentido contrario por 5 segundos
-    print("Gira el motor en sentido contrario")
-    GPIO.output(8, GPIO.LOW)
-    GPIO.output(10, GPIO.HIGH)
-    time.sleep(5)
+    x = raw_input()
 
-GPIO.cleanup()
+    if x == 'C':
+        print("Correr")
+        if (temp1 == 1):
+            GPIO.output(in1, GPIO.HIGH)
+            GPIO.output(in2, GPIO.LOW)
+            print("Av")
+            x = 'z'
+        else:
+            GPIO.output(in1, GPIO.LOW)
+            GPIO.output(in2, GPIO.HIGH)
+            print("Reversa")
+            x = 'z'
+
+
+    elif x == 'A':
+        print("Alto")
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.LOW)
+        x = 'z'
+
+    elif x == 'Av':
+        print("Avanzar")
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+        temp1 = 1
+        x = 'z'
+
+    elif x == 'R':
+        print("Reversa")
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
+        temp1 = 0
+        x = 'z'
+
+    elif x == 'L':
+        print("Low")
+        p.ChangeDutyCycle(25)
+        x = 'z'
+
+    elif x == 'M':
+        print("Medium")
+        p.ChangeDutyCycle(50)
+        x = 'z'
+
+    elif x == 'H':
+        print("High")
+        p.ChangeDutyCycle(75)
+        x = 'z'
 
 if __name__ == "__main__"
   main()
