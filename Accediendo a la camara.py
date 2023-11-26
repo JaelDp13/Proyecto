3bdccf9180d787bf556057e9df31e94251ed45d2
JaelDp13 <146679309+JaelDp13@users.noreply.github.com>
2023-11-26 14:55:11 -0600

    Update Accediendo a la camara.py

--- Accediendo a la camara.py
+++ Accediendo a la camara.py
@@ -1,11 +1,21 @@
 import cv2
-captura = cv2.VideoCapture(1)
-while (captura.isOpened()):
-  ret, imagen = captura.read()
-  if ret == True:
-    cv2.imshow('video', imagen)
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-      break
-  else: break
-captura.release()
-cv2.destroyAllWindows()
+
+class Camara:
+    def __init__(self, camara_index=0):
+        self.camara = cv2.VideoCapture(camara_index)
+
+    def _del_(self):
+        self.camara.release()
+
+    def show_camara(self):
+        while True:
+            ret, frame = self.camara.read()
+            cv2.imshow('Camara', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    my_camara = Camara()
+    my_camara.show_camara()
